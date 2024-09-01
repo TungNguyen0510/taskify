0d7afce60663ac6a2d0beb4c23b515f35395d753
tungnn <tn05102002@gmail.com>
2024-09-01 23:39:39 +0700

    fix: home page height

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -8,7 +8,7 @@ import { AppConfig } from '@/utils/AppConfig';
 export default function HomePage() {
   return (
     <div className="flex min-h-[calc(100vh-56px)] flex-col items-center">
-      <div className="mx-auto flex w-full flex-col items-center gap-4 bg-gray-100 p-6 text-center">
+      <div className="mx-auto flex size-full min-h-[calc(100vh-56px)] flex-col items-center gap-4 bg-gray-100 p-6 text-center">
         <div className="mt-8 text-wrap text-[66px] leading-[73px]">
           <span className="font-bold">Great outcomes</span> start with
           <span className="bg-gradient-to-r from-cyan-200 to-indigo-600 bg-clip-text font-semibold text-transparent hover:from-cyan-200 hover:to-fuchsia-500">
